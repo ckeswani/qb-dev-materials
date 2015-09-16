8dcf5a46c55da4377433bfa2c355798e20bad216
Keswani, Claude <Claude_Keswani@intuit.com>
2015-09-16 16:53:30 -0400

    implementing DBManager.getConnection()

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 ##Requirements
 
-##Running the code
+##Building/Running the code
+
+From the root dir, 
+`
+gradle jar
+java -jar build/libs/dev-interview-materials.jar
+`
--- src/main/java/com/intuit/quickbase/Main.java
+++ src/main/java/com/intuit/quickbase/Main.java
@@ -1,8 +1,18 @@
 package com.intuit.quickbase;
 
+import com.intuit.quickbase.devint.DBManagerImpl;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main( String args[] )
     {
-       System.out.println("Starting.");
+        System.out.print("Getting DB Connection...");
+        DBManagerImpl dbm = new DBManagerImpl();
+        Connection c = dbm.getConnection();
+        if (c == null) {
+            System.out.println("failed.");
+            System.exit(1);
+        }
     }
 }
