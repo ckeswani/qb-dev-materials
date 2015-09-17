be9562e42f991ed4e3a4dfc4166209c92b6c4541
renaissanceGeek <julie_sheffield@intuit.com>
2015-09-16 21:30:00 -0400

    Add the pseudo-API for returning country and population data

--- src/main/java/com/intuit/quickbase/Main.java
+++ src/main/java/com/intuit/quickbase/Main.java
@@ -1,8 +1,21 @@
 package com.intuit.quickbase;
 
+import com.intuit.quickbase.devint.ConcreteStatService;
+import com.intuit.quickbase.devint.IStatService;
+import java.util.List;
+import org.apache.commons.lang3.tuple.Pair;
+
 public class Main {
     public static void main( String args[] )
     {
        System.out.println("Starting.");
+       
+       IStatService service = new ConcreteStatService();
+       List<Pair<String, Integer>> countries = service.GetCountryPopulations();
+       for (Pair<String, Integer> pair : countries)
+       {
+    	  System.out.printf("%s: %d", pair.getLeft(), pair.getRight());
+    	  System.out.println();
+       }
     }
 }
